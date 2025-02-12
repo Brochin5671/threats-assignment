52ebca4ab9efc4956f5db3ada447e36c2a6388a1
Brochin5671 <maximbrochin@gmail.com>
2025-02-12 15:43:57 -0500

    Document functions

--- backend/src/get_threats.py
+++ backend/src/get_threats.py
@@ -2,11 +2,9 @@ import requests
 from .models import Threat
 
 
-def get_urlhaus_data(limit: int | None = None):
+def get_urlhaus_data():
+    """Calls the urlhaus-api and tries to return the response as a JSON."""
     url = 'https://urlhaus-api.abuse.ch/v1/urls/recent/'
-    # Get threats using a limit if provided
-    if limit:
-        url += f'limit/{limit}/'
 
     # Call urlhaus-api and return data as a JSON
     data = {}
@@ -15,11 +13,13 @@ def get_urlhaus_data(limit: int | None = None):
         res.raise_for_status()
         data = res.json()
     except requests.exceptions.RequestException as e:
-        data['error'] = str(e)  # TODO: handle err message
+        data['error'] = 'Something went wrong on our end.'
+        print(e)
     return data
 
 
 def extract_urlhaus_data(data) -> list[Threat]:
+    """Extracts required threat data from the JSON to return as a list."""
     # Extract just the host, url, threat type, and date added attributes from each object in JSON
     threats = []
     for item in data['urls']:
@@ -32,13 +32,15 @@ def extract_urlhaus_data(data) -> list[Threat]:
     return threats
 
 
-def paginate_threats_data(threats: list, page: int, limit: int) -> list[Threat]:
+def paginate_threats_data(threats: list[Threat], page: int, limit: int) -> list[Threat]:
+    """Returns a chunk of the threats list for pagination."""
     start = (page - 1) * limit
     end = start + limit
     return threats[start:end]
 
 
 def get_threats_data(page: int = 1, limit: int = 10):
+    """Tries to return the threat data for the /api/threats/ endpoint."""
     # Get data from API
     data = get_urlhaus_data()
     if data.get('error'):
