a1071557bdb9bcb620ca6cd42fcf6745edf776d7
Brochin5671 <maximbrochin@gmail.com>
2025-02-12 15:43:29 -0500

    Document

--- backend/main.py
+++ backend/main.py
@@ -11,17 +11,17 @@ CORS(app, origins=['http://localhost:5173'])  # TODO: list origin differently
 
 @app.get('/api/threats')
 def get_threats():
+    """Endpoint for getting a list of threats from urlhaus-api."""
     # Get query arguments and verify proper usage
     page = request.args.get('page', type=int)
     limit = request.args.get('limit', type=int)
     if (page is None) != (limit is None):
         return jsonify({'error': 'Need to specify both page and limit query arguments.'}), 400
 
-    # Try to get the threats list data
-    args = [page, limit]
     # Only pass arguments that are not None
+    args = [page, limit]
     data = get_threats_data(*[arg for arg in args if arg is not None])
-
+    # Try to get the threats list data
     if data.get('error'):
         return jsonify(data), 500
     return jsonify(data)
