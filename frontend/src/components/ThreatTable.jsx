bb273141b250b8f4d438ef4d5a57ea906255dd14
Brochin5671 <maximbrochin@gmail.com>
2025-02-12 00:25:39 -0500

    Memoize values

--- frontend/src/components/ThreatTable.jsx
+++ frontend/src/components/ThreatTable.jsx
@@ -1,30 +1,34 @@
 import { Paper } from '@mui/material'
 import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { useMemo } from 'react'
 
 export const ThreatTable = ({ threats, loading }) => {
-  const threatTypes = [
-    ...new Set(threats?.map((threat) => threat['threat_type'])),
-  ]
-
-  const columns = [
-    { field: 'host', headerName: 'Host', width: 200 },
-    { field: 'url', headerName: 'URL', width: 250 },
-    {
-      field: 'threat_type',
-      headerName: 'Threat Type',
-      width: 150,
-      type: 'singleSelect',
-      valueOptions: threatTypes,
-    },
-    {
-      field: 'date_added',
-      headerName: 'Date Added',
-      width: 175,
-      type: 'datetime',
-      valueGetter: (date) => date && new Date(date),
-      valueFormatter: (value) => value?.toLocaleString(), // TODO: fix issue with date filtering
-    },
-  ]
+  const threatTypes = useMemo(
+    () => [...new Set(threats?.map((threat) => threat['threat_type']))],
+    [threats]
+  )
+  const columns = useMemo(
+    () => [
+      { field: 'host', headerName: 'Host', width: 200 },
+      { field: 'url', headerName: 'URL', width: 250 },
+      {
+        field: 'threat_type',
+        headerName: 'Threat Type',
+        width: 150,
+        type: 'singleSelect',
+        valueOptions: threatTypes,
+      },
+      {
+        field: 'date_added',
+        headerName: 'Date Added',
+        width: 175,
+        type: 'datetime',
+        valueGetter: (date) => date && new Date(date),
+        valueFormatter: (value) => value?.toLocaleString(), // TODO: fix issue with date filtering
+      },
+    ],
+    [threatTypes]
+  )
 
   return (
     <Paper>
