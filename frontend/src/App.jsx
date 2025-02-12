f6eefc7d587a7dfb44d791bb5aedae9e69c12deb
Brochin5671 <maximbrochin@gmail.com>
2025-02-12 10:23:50 -0500

    Move logic to ThreatTable and paginate based off server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,11 @@ import { ThreatTable } from './components/ThreatTable'
 import axios from 'axios'
 
 function App() {
-  const [threats, setThreats] = useState([])
-  const [loading, setLoading] = useState(false)
-
-  // Get threats data from API service
-  useEffect(() => {
-    setLoading(true)
-    axios
-      .get('http://localhost:5000/api/threats?page=1&limit=10')
-      .then((res) => {
-        setThreats(res.data['threats'])
-      })
-      .catch((err) => {
-        setThreats([])
-        console.error(err)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, [])
-
   return (
     <>
       <h1>Threats</h1>
       <div className="card">
-        <ThreatTable threats={threats} loading={loading} />
+        <ThreatTable />
       </div>
     </>
   )
--- frontend/src/components/ThreatTable.jsx
+++ frontend/src/components/ThreatTable.jsx
@@ -1,8 +1,37 @@
 import { Paper } from '@mui/material'
 import { DataGrid, GridToolbar } from '@mui/x-data-grid'
-import { useMemo } from 'react'
+import { useMemo, useState, useEffect } from 'react'
+import axios from 'axios'
 
-export const ThreatTable = ({ threats, loading }) => {
+export const ThreatTable = () => {
+  const [threats, setThreats] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(10)
+  const [length, setLength] = useState(0)
+
+  const getThreats = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get(
+        `http://localhost:5000/api/threats?page=${page}&limit=${limit}`
+      ) // TODO: get API route another way
+      setThreats(res.data['threats'])
+      setLength(res.data['length'])
+    } catch (err) {
+      setThreats([])
+      setLength(0)
+      console.error(err)
+    }
+    setLoading(false)
+  }
+
+  // Get threats data from API service
+  useEffect(() => {
+    getThreats()
+  }, [page, limit])
+
   const threatTypes = useMemo(
     () => [...new Set(threats?.map((threat) => threat['threat_type']))],
     [threats]
@@ -42,6 +71,12 @@ export const ThreatTable = ({ threats, loading }) => {
         sx={{ border: 0 }}
         slots={{ toolbar: GridToolbar }}
         loading={loading}
+        onPaginationModelChange={(model) => {
+          setPage(model.page + 1)
+          setLimit(model.pageSize)
+        }}
+        paginationMode="server"
+        rowCount={length}
       />
     </Paper>
   )
