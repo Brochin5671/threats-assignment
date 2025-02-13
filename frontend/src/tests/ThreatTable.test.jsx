e2021885975e243e7b1fe7070b6e40feb2555392
Brochin5671 <maximbrochin@gmail.com>
2025-02-12 19:02:41 -0500

    Complete tests for frontend

--- frontend/src/tests/ThreatTable.test.jsx
+++ frontend/src/tests/ThreatTable.test.jsx
@@ -1,8 +1,52 @@
-import { render } from '@testing-library/react'
+import { render, screen, cleanup, waitFor } from '@testing-library/react'
 import { ThreatTable } from '../components/ThreatTable'
+import { afterEach, beforeEach, expect, it, vi } from 'vitest'
+import axios from 'axios'
+
+// Mock Axios
+vi.mock('axios')
+afterEach(cleanup)
 
 describe('ThreatTable', () => {
-  it('renders the ThreatTable component', () => {
+  it('displays filled table', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: {
+        threats: [
+          {
+            id: 1,
+            host: 'evil',
+            url: 'evil.com',
+            threat_type: 'bad',
+            date_added: '1/1/2024 00:00:00 UTC',
+          },
+        ],
+        length: 1,
+      },
+    })
     render(<ThreatTable />)
+    await waitFor(() => {
+      expect(screen.getByText('evil')).toBeInTheDocument()
+    })
+  })
+
+  it('displays empty table', async () => {
+    axios.get.mockResolvedValueOnce({
+      data: {
+        threats: [],
+        length: 0,
+      },
+    })
+    render(<ThreatTable />)
+    await waitFor(() => {
+      expect(screen.getByText('No rows')).toBeInTheDocument()
+    })
+  })
+
+  it('displays empty table from axios error', async () => {
+    axios.get.mockRejectedValueOnce(new Error('Axios mock test failure'))
+    render(<ThreatTable />)
+    await waitFor(() => {
+      expect(screen.getByText('No rows')).toBeInTheDocument()
+    })
   })
 })
